3381636956d5a4d003b35654ac4e36ddb0f20d60
sfkelseylee <138422247+sfkelseylee@users.noreply.github.com>
2026-08-05 12:29:43 +0800

    Update namespace for HasAffixActions contract

--- src/TimeRangeSlider.php
+++ src/TimeRangeSlider.php
@@ -7,7 +7,7 @@ use Closure;
 use Filament\Forms\Components\Concerns\HasAffixes;
 use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
-use Filament\Forms\Components\Contracts\HasAffixActions;
+use Filament\Schemas\Components\Contracts\HasAffixActions;
 use Filament\Forms\Components\Field;
 use Filament\Support\Concerns\HasExtraAlpineAttributes;
 
